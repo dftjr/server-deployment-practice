72360a9d3e772ed3e7c26677378756087224a0f9
DAVID TUSIA <DAVE.TUSIA@GMAIL.COM>
2022-09-20 09:06:24 +0000

    Initial setup

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+require('dotenv').config();
+const app = require('./server.js');
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log('App is running');
+});
