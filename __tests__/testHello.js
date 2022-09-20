53b4926fd1710b4b0f4d8c6928f00a983ed28bae
David Tusia <105877296+dftjr@users.noreply.github.com>
2022-09-20 02:20:56 -0700

    Update testHello.js

--- __tests__/testHello.js
+++ __tests__/testHello.js
@@ -8,19 +8,19 @@ describe('Testing our HTTP Server', () => {
     test('Should return an object with a name property, on GET to /hello', async () => {
         let response = await request.get('/hello');
         expect(response.status).toEqual(200);
-        expect(response.body.name).toEqual('JACOB');
+        expect(response.body.name).toEqual('DAVID');
     });
 
     test('Should return an object with a name property, on GET to /params/:name', async () => {
-        let response = await request.get('/params/jacob');
+        let response = await request.get('/params/david');
         expect(response.status).toEqual(200);
-        expect(response.body.name).toEqual('jacob');
+        expect(response.body.name).toEqual('david');
     });
 
-    test('Should return an object with a name property and role property, on GET to /query?name=jacob&role=instructor', async () => {
-        let response = await request.get('/query?name=Jacob&role=instructor');
+    test('Should return an object with a name property and role property, on GET to /query?name=david&role=student', async () => {
+        let response = await request.get('/query?name=David&role=student');
         expect(response.status).toEqual(200);
-        expect(response.body.name).toEqual('Jacob');
-        expect(response.body.role).toEqual('instructor');
+        expect(response.body.name).toEqual('David');
+        expect(response.body.role).toEqual('student');
     });
 });
